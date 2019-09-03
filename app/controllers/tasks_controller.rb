39df11a6d18f0036820f9250c482e6a0cc37e6fd
test1234 <zzzmuek@mineo.jp>
2019-09-03 16:00:51 +0000

    CRUD messages

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,59 @@
+class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'タスクが正常に登録されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが登録されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'タスクは正常に更新されました'
+      redirect_to @task
+    
+    else
+      flash.now[:danger] = 'タスクは更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to tasks_url
+  end
+
+private
+
+# Strong Parameter
+def task_params
+  params.require(:task).permit(:content)
+end
+
+end
