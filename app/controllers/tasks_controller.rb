57fc3b142886dffab98f6456f77198b65d2e3a61
test1234 <zzzmuek@mineo.jp>
2019-09-04 06:34:18 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -25,12 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
@@ -42,18 +39,21 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
   end
 
-private
+  private
 
-# Strong Parameter
-def task_params
-  params.require(:task).permit(:content)
-end
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_130632) do
+ActiveRecord::Schema.define(version: 2019_09_04_051603) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
