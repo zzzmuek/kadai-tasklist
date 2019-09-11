67f19551155e1bcac7da1a25ca38308dcf1c5fba
test1234 <zzzmuek@mineo.jp>
2019-09-11 23:42:43 +0000

    top page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+(1..50).each do |number|
+	Task.create!(status: 'test' + number.to_s, content: 'test content' + number.to_s)
+end
