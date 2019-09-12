665d24eb14be887fd745e8debd42f236a90ead08
test1234 <zzzmuek@mineo.jp>
2019-09-12 04:08:24 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'tasks#index'  
   
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
